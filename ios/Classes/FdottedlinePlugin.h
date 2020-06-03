64b6031d607f6ad1003f3b945a39730917025768
纽特 <coorchice.cb@alibaba-inc.com>
2020-06-03 18:40:15 +0800

    other#1.0.0#update .gitignore

--- ios/Classes/FdottedlinePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FdottedlinePlugin : NSObject<FlutterPlugin>
-@end
--- test/fdottedline_test.dart
+++ test/fdottedline_test.dart
@@ -3,21 +3,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fdottedline/fdottedline.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('fdottedline');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await Fdottedline.platformVersion, '42');
-  });
+//  const MethodChannel channel = MethodChannel('fdottedline');
+//
+//  TestWidgetsFlutterBinding.ensureInitialized();
+//
+//  setUp(() {
+//    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//      return '42';
+//    });
+//  });
+//
+//  tearDown(() {
+//    channel.setMockMethodCallHandler(null);
+//  });
+//
+//  test('getPlatformVersion', () async {
+//    expect(await Fdottedline.platformVersion, '42');
+//  });
 }
